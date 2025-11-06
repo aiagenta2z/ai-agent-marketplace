8724151158b71a4fe4124caf8fa3ca909876451b
AI-Hub-Admin <aihubadmin@126.com>
2025-11-06 12:35:45 +0800

    update new AI Agent marketplace source code...

---
+++ src/ai_agent_marketplace/constants.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+KEY_ID = "id"
+KEY_GITHUB = "github"
+KEY_TIMEOUT = "timeout"
+KEY_SERVER_IDS = "server_ids"
+KEY_CONFIG_NAME = "config_name"
+KEY_URL = "url"
+
+KEY_ACCESS_KEY = "access_key"
+KEY_CONTENT_NAME = "content_name"
+KEY_NAME = "name"
+
+KEY_ENDPOINT_REGISTER_V2 = "deepnlp_register_v2"
+KEY_ENDPOINT_REGISTER_V1 = "deepnlp_register_v1"
+
+KEY_ENDPOINT_BASE_URL_V2 = "https://www.deepnlp.org/api/ai_agent_marketplace/v2"
+KEY_ENDPOINT_REGISTER_V2_URL = "https://www.deepnlp.org/api/ai_agent_marketplace/registry"
+KEY_ENDPOINT_REGISTER_V1_URL = "https://www.deepnlp.org/api/ai_agent_marketplace/v1"
+KEY_ENDPOINT_BASE_URL_AIAGENTA2Z_V2 = "https://www.aiagenta2z.com/api/ai_agent_marketplace/v2"
+
+
+DEFAULT_TIMEOUT = 10
+
+KEY_AI_AGENT_MARKETPLACE_API_KEY = "AI_AGENT_MARKETPLACE_API_KEY"
+
+MOCK_ACCESS_KEY = "${your_access_key}"
